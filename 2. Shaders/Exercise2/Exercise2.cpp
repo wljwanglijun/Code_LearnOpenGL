ee0bbf22e286a0dd32afc081f2c2fc36dc85fcce
Lijun Wang <>
2022-03-03 22:23:33 +0800

    LearnOpenGL/Getting started/Shaders/Exercise2 code

---
+++ 2. Shaders/Exercise2/Exercise2.cpp
@@ -0,0 +1,120 @@
+#include <glad/glad.h> //glad头文件添加成功 写在前面 不然会报错
+#include <GLFW/glfw3.h> //GLFW头文件添加成功
+#include "shader_s.h"
+#include <iostream>
+
+//函数声明
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+//常量定义
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+int main(int argc, const char * argv[]) {
+
+    glfwInit(); //glfw库初始化，在使用glfw函数前，必须先初始化
+    //以下均为glfw配置，第一个参数为配置的选项，以GLFW_为前缀，第二个参数为配置的z值
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); //opengl版本设置
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); //opengl版本设置
+    glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE); //使用opengl core profile配置
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); //苹果系统需要特别加的一句
+
+    //创建窗口
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if(window == NULL)
+    {
+        std::cout << "Filed to create GLFW window" << std::endl; //有了std::，就不需要在最前面写using namespace std
+        glfwTerminate();
+        return -1;
+    }
+    //使创建的window上下文为当前线程的主上下文 意思是之后所有的绘制都是在window这个窗口下
+    glfwMakeContextCurrent(window);
+    //当window大小发生变化时，viewport大小也跟着同步变化 设置视口大小
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    //GLAD初始化 GLAD加载OpenGL函数的指针
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Filed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    //build & compile shader & link shader program
+    Shader ourShader("shader.vs", "shader.fs");
+
+    //vertex data
+    float vertices[]={
+            //first triangle
+            0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+            -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+            0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f
+    };
+
+    //vertex buffer
+    unsigned int VBO;//opengl object:vertex buffer object 将顶点数据一次性从gpu发送至图卡内存
+    unsigned int VAO; //opengl object:vertex array object 将顶点属性设置、顶点属性状态及使用哪个VBO画储存起来
+    //unsigned int EBO; //opengl object:element buffer object 将顶点索引储存起来 避免相同顶点在不同三角形中的重复绘制
+    glGenVertexArrays(1, &VAO);//生成VAO
+    glGenBuffers(1, &VBO);//生成VBO
+
+    //first triangle
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER,VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    //顶点属性
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    //颜色属性
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)(3*sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    //uniform
+    float offset = 0.5f;
+
+    //render loop 一直不停的绘制直至结束
+    while(!glfwWindowShouldClose(window)) //每一次循环开始时都检查是否需要关闭窗口
+    {
+        //按键输入监测
+        processInput(window);
+
+        //绘制指令
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f); //确定用什么颜色来清理屏幕
+        glClear(GL_COLOR_BUFFER_BIT); //确定清理哪一个缓冲区，这里是用上面的颜色来清理颜色缓冲区
+
+        //画三角形
+        ourShader.use(); //激活shaderProgram，应用shaderProgram附加的着色器进行绘制
+        ourShader.setFloat("xOffset", offset);
+
+        glBindVertexArray(VAO); //绑定VAO
+        glDrawArrays(GL_TRIANGLES, 0, 3); //绘制
+
+        //事件响应并交换前后缓冲区
+        glfwSwapBuffers(window);//交换前后缓冲区，在屏幕上显示图像
+        glfwPollEvents(); //检查是否有事件触发 更新window状态 响应回调函数中的自定义功能
+    }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+
+    //规范的退出应用 释放glfw占用的内存资源
+    glfwTerminate();
+    return 0;
+}
+
+
+//回调函数：当window大小发生变化时，viewport大小也跟着同步变化
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0,0,width,height);//前两个参数是左下角坐标，后两个参数是宽/高，数值代表像素
+}
+
+//键盘输入相应函数
+void processInput(GLFWwindow* window)
+{
+    //如果按下escape键，将window设置为关闭属性，glfwWindowShouldClose(window)返回true，render loop结束，窗口关闭
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
