4e60ac190f78239cc5c0ca6277a3a28b57a54ee4
Lijun Wang <>
2022-04-26 17:21:57 +0800

    LearnOpenGL/Lighting/LightingMap/LightingMap code

---
+++ 2. Lighting/4. Lighting Maps/LightingMaps/lighting_map.fs
@@ -0,0 +1,51 @@
+#version 330 core
+
+out vec4 FragColor;
+
+in vec3 FragPos;
+in vec3 Normal;
+in vec2 TexCoords;
+
+struct Material {
+    sampler2D diffuse; //此处材质无ambient属性，是因为通常情况下ambient为diffuse RGB三通道等比例减小的结果，相当于有了diffuse，就有了ambient
+    sampler2D specular; //对于Light.pecular的反射率
+    float shininess; //镜面反射形成的光点的聚焦程度，越大越聚焦，越小越涣散
+};
+
+struct Light {
+    vec3 position; //灯光位置属性
+    vec3 ambient; //灯光环境光颜色属性 一般与diffuse相同，或者RGB同比率减小
+    vec3 diffuse; //灯光漫反射颜色属性 本身的颜色
+    vec3 specular; //灯光镜面反射光颜色属性 一般为白光 1 1 1
+};
+
+
+//uniform变量允许在应用程序中修改进行修改
+uniform Material material; //材质属性
+uniform Light light; //灯光属性
+uniform vec3 viewPos; //视角（相机）位置
+
+void main()
+{
+        //物体表面显示的最终颜色是灯光和材质共同作用的结果
+        // ambient
+        vec3 ambient = light.ambient * vec3(texture(material.diffuse,TexCoords));
+
+        // diffuse
+        vec3 norm = normalize(Normal);
+        vec3 lightDir = normalize(light.position - FragPos);
+        float diff = max(dot(norm, lightDir), 0.0);
+        vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse,TexCoords));
+
+
+        // specular
+        vec3 viewDir = normalize(viewPos - FragPos);
+        vec3 reflectDir = reflect(-lightDir, norm);
+        float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+        vec3 specular = light.specular * spec * vec3(texture(material.specular, TexCoords));
+
+        //ambient diffuse specular三者相加，得到最后的颜色
+        vec3 result = ambient + diffuse + specular;
+
+        FragColor = vec4(result, 1.0);
+}
---
+++ 2. Lighting/4. Lighting Maps/LightingMaps/main.cpp
@@ -0,0 +1,382 @@
+#include <glad/glad.h> //glad头文件添加成功 写在前面 不然会报错
+#include <GLFW/glfw3.h> //GLFW头文件添加成功
+#include "stb_image.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include "shader_s.h"
+#include "camera.h"
+#include <iostream>
+
+
+//函数声明
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+void mouse_callback(GLFWwindow* window, double x_mouse, double y_mouse);
+void scroll_callback(GLFWwindow* window, double x_mouse, double y_mouse);
+unsigned int loadTexture(const char *path);
+
+//常量定义
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+//帧渲染时间
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+//相机初始化
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = SCR_WIDTH/2;
+float lastY = SCR_HEIGHT/2; //设置初始鼠标位置为窗口中心
+bool firstMouse = true;
+
+//灯光位置
+glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+
+//纹理贴图路径
+//diffuse map
+const char diffuseMapPath[] = "../Texture/container_diffuseMap.png";
+const char *path_diffuseMap = diffuseMapPath;
+//specular map
+const char specularMapPath[] = "../Texture/container_specularMap.png";
+const char *path_specularMap = specularMapPath;
+
+
+int main(int argc, const char * argv[]) {
+
+    glfwInit(); //glfw库初始化，在使用glfw函数前，必须先初始化
+    //以下均为glfw配置，第一个参数为配置的选项，以GLFW_为前缀，第二个参数为配置的z值
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); //opengl版本设置
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); //opengl版本设置
+    glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE); //使用opengl core profile配置
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); //苹果系统需要特别加的一句
+
+    //创建窗口
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if(window == NULL)
+    {
+        std::cout << "Filed to create GLFW window" << std::endl; //有了std::，就不需要在最前面写using namespace std
+        glfwTerminate();
+        return -1;
+    }
+    //使创建的window上下文为当前线程的主上下文 意思是之后所有的绘制都是在window这个窗口下
+    glfwMakeContextCurrent(window);
+    //当window大小发生变化时，viewport大小也跟着同步变化 设置视口大小
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    //当鼠标位置移动后，调用mouse_callback函数
+    glfwSetCursorPosCallback(window, mouse_callback);
+    //当鼠标滚轮移动后，调用scroll_callback函数
+    glfwSetScrollCallback(window, scroll_callback);
+
+    //capture the mouse
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+
+    //GLAD初始化 GLAD加载OpenGL函数的指针
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Filed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    //启用深度检测
+    glEnable(GL_DEPTH_TEST);
+
+    //build & compile shader & link shader program
+    Shader lightingShader("../Shader/lighting_map.vs", "../Shader/lighting_map.fs"); //返回上一级菜单../
+    Shader lightCubeShader("../Shader/light_cube.vs", "../Shader/light_cube.fs");
+
+    //vertex data
+    float vertices[]={
+            //顶点坐标+法向量+纹理坐标 cube
+            -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f, 0.0f, 0.0f,
+            0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f, 1.0f, 0.0f,
+            0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f, 1.0f, 1.0f,
+            0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f, 1.0f, 1.0f,
+            -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f, 0.0f, 1.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f, 0.0f, 0.0f,
+
+            -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f, 0.0f, 0.0f,
+            0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f, 1.0f, 0.0f,
+            0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f, 1.0f, 1.0f,
+            0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f, 1.0f, 1.0f,
+            -0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f, 0.0f, 1.0f,
+            -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f, 0.0f, 0.0f,
+
+            -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f, 1.0f, 0.0f,
+            -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f, 1.0f, 1.0f,
+            -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f, 0.0f, 1.0f,
+            -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f, 0.0f, 1.0f,
+            -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f, 0.0f, 0.0f,
+            -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f, 1.0f, 0.0f,
+
+            0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f, 1.0f, 0.0f,
+            0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f, 1.0f, 1.0f,
+            0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f, 0.0f, 1.0f,
+            0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f, 0.0f, 1.0f,
+            0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f, 0.0f, 0.0f,
+            0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f, 1.0f, 0.0f,
+
+            -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f, 0.0f, 1.0f,
+            0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f, 1.0f, 1.0f,
+            0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f, 1.0f, 0.0f,
+            0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f, 1.0f, 0.0f,
+            -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f, 0.0f, 0.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f, 0.0f, 1.0f,
+
+            -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f, 0.0f, 1.0f,
+            0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f, 1.0f, 1.0f,
+            0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f, 1.0f, 0.0f,
+            0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f, 1.0f, 0.0f,
+            -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f, 0.0f, 0.0f,
+            -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f, 0.0f, 1.0f
+    };
+
+    //vertex buffer
+    //cubeVAO设置
+    unsigned int VBO;//opengl object:vertex buffer object 将顶点数据一次性从gpu发送至图卡内存
+    unsigned int cubeVAO; //opengl object:vertex array object 将顶点属性设置、顶点属性状态及使用哪个VBO画储存起来
+    glGenVertexArrays(1, &cubeVAO);//生成VAO
+    glGenBuffers(1, &VBO);//生成VBO
+    //绑定VBO，顶点数据发送至显存
+    glBindBuffer(GL_ARRAY_BUFFER,VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    //绑定VAO
+    glBindVertexArray(cubeVAO);
+    //顶点属性
+    //顶点位置
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    //顶点法线
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    //顶点纹理坐标
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    unsigned int lightCubeVAO;
+    glGenVertexArrays(1, &lightCubeVAO);
+    glBindVertexArray(lightCubeVAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    // note that we update the lamp's position attribute's stride to reflect the updated buffer data
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+
+    //load diffuse map
+    unsigned int diffuseMap = loadTexture(path_diffuseMap);
+    //load specular map
+    unsigned int specularMap = loadTexture(path_specularMap);
+    //设置片段着色器diffuseMap变量值
+    lightingShader.use();
+    lightingShader.setInt("material.diffuse", 0); //只有一个纹理单元的时候，该纹理单元的索引默认为0
+    lightingShader.setInt("material.specular", 1);
+
+
+    //render loop 一直不停的绘制直至结束
+    while(!glfwWindowShouldClose(window)) //每一次循环开始时都检查是否需要关闭窗口
+    {
+        //计算前后帧间隔时间
+        float currentFrame = 0.0;
+        currentFrame = static_cast<float>(glfwGetTime());
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        //按键输入监测
+        processInput(window);
+
+        //绘制指令
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f); //确定用什么颜色来清理屏幕
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //确定清理哪一个缓冲区，这里是用上面的颜色来清理颜色缓冲区
+
+        //设置立方体颜色绘制着色器（本身颜色*灯光颜色）
+        lightingShader.use();
+        //设置视点位置输入 计算specular
+        lightingShader.setVec3("viewPos", camera.Position);
+        //设置立方体材质属性 emerald 翡翠材质
+        lightingShader.setVec3("material.specular", 0.5f, 0.5f, 0.5f);
+        lightingShader.setFloat("material.shininess", 64.0f);
+        //灯光属性设置
+        lightingShader.setVec3("light.position", lightPos);
+        lightingShader.setVec3("light.ambient", 0.2f, 0.2f, 0.2f);
+        lightingShader.setVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
+        lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
+
+        //投影变换矩阵
+        glm::mat4 projection;
+        projection = glm::perspective(glm::radians(camera.Zoom),(float)SCR_WIDTH/(float)SCR_HEIGHT, 0.1f, 100.0f);
+        //投影变换传递给顶点着色器
+        lightingShader.use();
+        lightingShader.setMat4("projection", projection);
+
+        //视角变换矩阵
+        glm::mat4 view = glm::mat4(1.0f);
+        //lookat的第二个参数表示相机始终看向当前位置的向Z轴减1的位置（无论相机位置怎么变，危机始终看向当前z轴-1）
+        view = camera.GetViewMatrix();
+        lightingShader.setMat4("view", view);
+
+        //模型变换
+        glm::mat4 model = glm::mat4(1.0f);
+        lightingShader.setMat4("model", model);
+
+        //激活纹理单元
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, diffuseMap);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, specularMap);
+
+        //绘制带颜色的立方体
+        glBindVertexArray(cubeVAO); //绑定VAO
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+
+        //设置灯光立方体的mvp变换
+        lightCubeShader.use();
+        lightCubeShader.setMat4("projection", projection);
+        lightCubeShader.setMat4("view", view);
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, lightPos);
+        model = glm::scale(model, glm::vec3(0.2f));
+        lightCubeShader.setMat4("model", model);
+
+        //绘制灯光立方体
+        glBindVertexArray(lightCubeVAO); //绑定VAO
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+
+        //事件响应并交换前后缓冲区
+        glfwSwapBuffers(window);//交换前后缓冲区，在屏幕上显示图像
+        glfwPollEvents(); //检查是否有事件触发 更新window状态 响应回调函数中的自定义功能
+    }
+
+    glDeleteVertexArrays(1, &cubeVAO);
+    glDeleteVertexArrays(1, &lightCubeVAO);
+    glDeleteBuffers(1, &VBO);
+
+    //规范的退出应用 释放glfw占用的内存资源
+    glfwTerminate();
+
+    return 0;
+}
+
+
+//回调函数：当window大小发生变化时，viewport大小也跟着同步变化
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0,0,width,height);//前两个参数是左下角坐标，后两个参数是宽/高，数值代表像素
+}
+
+//键盘输入相应函数
+void processInput(GLFWwindow* window)
+{
+    //如果按下escape键，将window设置为关闭属性，glfwWindowShouldClose(window)返回true，render loop结束，窗口关闭
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
+
+    //键盘控制相机移动
+    if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+    {
+        camera.ProcessKeyboardInput(FORWARD, deltaTime);
+    }
+    if(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+    {
+        camera.ProcessKeyboardInput(BACKWARD, deltaTime);
+    }
+    if(glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+    {
+        camera.ProcessKeyboardInput(LEFT, deltaTime);
+    }
+    if(glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+    {
+        camera.ProcessKeyboardInput(RIGHT, deltaTime);
+    }
+}
+
+
+void mouse_callback(GLFWwindow* window, double x_mouse, double y_mouse)
+{
+    float xPos = static_cast<float>(x_mouse);
+    float yPos = static_cast<float>(y_mouse);
+
+    //避免初始获取鼠标位置时的相机瞬间跳转
+    if(firstMouse)
+    {
+        lastX = xPos;
+        lastY = yPos;
+        firstMouse = false;
+    }
+
+    //计算前后帧鼠标的位移量
+    float xOffset = xPos - lastX;
+    float yOffset = yPos - lastY;
+    lastX = xPos;
+    lastY = yPos;
+
+    camera.ProcessMouseInput(xOffset, yOffset);
+}
+
+void scroll_callback(GLFWwindow* window, double x_mouse, double y_mouse)
+{
+    camera.ProcessScrollInput(static_cast<float>(y_mouse));
+}
+
+unsigned int loadTexture(const char *path)
+{
+    //定义纹理对象
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+
+    //texture图像导入
+    int width, height, nrComponents;
+    unsigned char *data = stbi_load(path, &width, &height, &nrComponents, 0);
+
+    if(data)
+    {
+        //确定texture的颜色通道数
+        GLenum format;
+        if(nrComponents == 1)
+        {
+            format = GL_RED;
+        }
+        else if(nrComponents == 3)
+        {
+            format = GL_RGB;
+        }
+        else if(nrComponents == 4)
+        {
+            format = GL_RGBA;
+        }
+
+        //纹理对象绑定
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        //纹理图片绑定
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        //生成mipmap贴图
+        //欲解决问题：当物体贴图分辨率较高，且距离视点位置较远时，此时该物体占据的像素单元较少，一个像素单元会对应多个顶点，每个
+        //顶点对应的颜色不同，opengl无法确定该使用哪一个颜色
+        //目的：物体距离视点位置的变化，表面贴图分辨率的自适应调整
+        //实现：一组基于原有贴图生成的分辨率逐渐减小的贴图序列，根据距离变化，使用最近邻/线性选择对应的贴图，并进行纹理采样
+        //这一系列图片之间分辨率4倍4倍的下降，实际操作过程是上一级图片上下左右四个像素合并为下一级图片的4个像素
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        //设置纹理环绕
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        //设置纹理过滤
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        //图片内存释放
+        stbi_image_free(data);
+    }
+    else
+    {
+        std::cout << "Texture failed to load at path: " << path << std::endl;
+        stbi_image_free(data);
+    }
+
+    return textureID;
+}
